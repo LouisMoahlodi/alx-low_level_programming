13cb598b8eb5f30fe32be71da43bbd1fc85ab40f
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-09 01:54:52 +0000

    Added task 4 file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing a char using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char Newstr[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(Newstr);
+	
+	return 0;
+}
