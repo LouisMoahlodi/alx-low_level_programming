67688f480ff1654e6956405e3126f36817cd2b59
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-09 03:57:06 +0000

    Added task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	long long typeoflonglong;
 
 	printf("Size of an int: %zu byte(s)\n", sizeof(typeofint));
-	rintf("Size of a char: %zu byte(s)\n", sizeof(typeofchar));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(typeolong));
+	printf("Size of a char: %zu byte(s)\n", sizeof(typeofchar));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(typeoflong));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(typeoflonglong));
-	printf("Size of a float: %zu byte(s)\n" sizeof(typeoffloat));
+	printf("Size of a float: %zu byte(s)\n", sizeof(typeoffloat));
 	return (0);
 }
