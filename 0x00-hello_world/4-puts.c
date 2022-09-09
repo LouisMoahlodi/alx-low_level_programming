f30c2098e3df75dbfb3ac388629bece7e3e9f179
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-09 02:04:42 +0000

    Correction 2 on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char Newstr[] = "Programming is like building a multilingual puzzle";
+	char Newstr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Newstr);
 	
