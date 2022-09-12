f97f87b41100c31655a5fdd7e9c157e225db28f6
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-12 17:13:08 +0000

    Correction on task 5 file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - Print all single digit numbers from base 10
  *
  * Return: Always 0 (Success)
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num < 10; num++)
 	{
 		printf("%d", num);
