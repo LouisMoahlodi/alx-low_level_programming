389da1c72e1e178604a98273e765211af3701f88
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-16 19:01:14 +0000

    Correction on task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Print all possible different combinations of 3 digits.
- *  
+ *
  *  The 3 digits must be different.
  *  012, 120, 102, 021, 201, 210 are considered the same combination.
  *  print only the smallest combination of 3 digits.
- *   
+ *
  *   Can only use `putchar` to print to console.
  *   Can only use `putchar` up to 6 times.
  *   Not allowed to use any variables of type `char`.
@@ -18,9 +18,9 @@ int main(void)
 
 	for (x = 0; x < 1000; x++)
 	{
-		y = x / 100; 
-		z = (x / 10) % 10; 
-		a = x % 10; 
+		y = x / 100;
+		z = (x / 10) % 10;
+		a = x % 10;
 
 		if (y < z && z < a)
 		{
---
+++ 0x02-functions_nested_loops/print_alpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alpha - print alphabet except c and k
+ *
+ * Return: void
+ */
+void print_alpha(void)
+{
+	int i;
+
+	for (i = 'a; i <= 'z'; i++)
+	{
+		if (i == 'c' || i i == 'k')
+			_putchar(i);
+		continue;
+	}
+	return;
+}
