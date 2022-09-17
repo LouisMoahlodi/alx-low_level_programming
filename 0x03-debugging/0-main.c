d2c541691f3e0aa64870a4f117bc9ff0376ed9c7
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-17 01:10:19 +0000

    Added all task files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test that the function is positive or negative
+ *
+ * Return: 0 ALways (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	postive_or_negative(i);
+
+	return (0);
+}
