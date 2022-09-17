9762f1e24b7fa53e808ee3244324c5bd8399847b
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-17 00:18:10 +0000

    Correction 2 on task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,26 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x <= 100; x++)
+	x = 1;
+	printf("%d", x);
+	for (x = 2; x <= 100; x++)
 	{
-		if (x % 15 == 0)
-			printf("Fizzbuzz");
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf(" Fizzbuzz");
+		}
 		else if (x % 3 == 0)
-			printf("Fizz");
+		{
+			printf(" Fizz");
+		}
 		else if (x % 5 == 0)
-			printf("Buzz");
+		{
+			printf(" Buzz");
+		}
 		else
-			printf("%d", x);
-		if (x < 100)
-			printf(" ");
+		{
+			printf(" %d", x);
+		}
 	}
 	printf("\n");
 	return (0);
