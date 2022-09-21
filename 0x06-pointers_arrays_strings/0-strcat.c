0d6c701c1c1b168986ccac2117e61153a4d930c0
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-21 16:58:13 +0000

    Added task 0 file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenate two strings 
+ *
+ * @dest - Destination: char variable 
+ * @src - Soruce: char varible
+ * @n: Is an intger
+ *
+ * Return: 0 Always (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0, y = 0;
+
+	while (dest[x] != '\0')
+		x++;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		y++;
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
