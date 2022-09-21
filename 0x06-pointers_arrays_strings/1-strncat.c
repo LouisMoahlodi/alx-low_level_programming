37d5ee4dda566be0f9f461d6cde2a4097c0bc467
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-21 20:11:04 +0000

    Correction 4 on task 1 file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
-/** _strncat - Function contcantenates two strings 
+/**
+ * _strncat - Function contcantenates two strings
  *
  * @dest: Destination pointer string
  * @src: Source pointer string
+ * @n: Is an integer
  *
- * Return 0 Always (Success)
+ * Return: 0 Always (Success)
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -15,9 +17,9 @@ char *_strncat(char *dest, char *src, int n)
 	len1 = strlen(dest);
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-	
+
 		dest[len1 + i] = src[i];
-	}	
+	}
 	dest[len1 + i] = '\0';
 	return (dest);
 }
