8fbd97aa7483cc060c69ff237d7cbf119771fbc1
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-22 10:51:01 +0000

    Correction 6 on task 3 file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int value = 0;
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 
 		if (s1[i] != s2[i])
@@ -25,8 +25,10 @@ int _strcmp(char *s1, char *s2)
 
 	if (value == 0)
 
-		return (s1[i]);
+		return (0);
 
+	else if (s1[i] > s2[i])
+		return (15);
 	else
-		return (s1[i]);
+		return (-15);
 }
