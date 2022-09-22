61fd436f9ac07ad1aee77cba969c7997e0d80ab8
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-22 10:25:29 +0000

    Correction 5 on task 3 file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,24 @@
  * Return: 1 if strings the same; 0 if not the same
  */
 int _strcmp(char *s1, char *s2)
-{
+{ 
 	int value = 0;
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{	
 
 		if (s1[i] != s2[i])
 		{
 			value = 1;
 			break;
 		}
+	}
+	
 	if (value == 0)
-		return (0);
+	
+		return (s1[i]);
+	
 	else
-		return ();
+		return (s1[i]);
 }
