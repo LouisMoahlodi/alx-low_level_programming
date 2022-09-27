a8ac163fbfc8c4ec9ded3940aea26e8283e38fa6
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-09-27 11:43:39 +0000

    Correction 10 on task 5 file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0;
 	int j = 0;
 
-	while (haystack[i] != '\0' || needle[j] != '\0')
+	while (haystack[i] != '\0' && needle[j] != '\0')
 	{
 		if (haystack[i] != needle[j])
 		{
