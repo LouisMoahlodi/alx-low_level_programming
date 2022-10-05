a3c8c1dc8bb2542084fd9dc6bc46c681451518e1
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-10-05 21:11:35 +0000

    Correction on task 2 file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,11 +21,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = NULL;
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2 = NULL;
 	}
 	new = malloc(sizeof(char) * (len1 + len2 + 1));
 
