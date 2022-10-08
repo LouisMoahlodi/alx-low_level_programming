895fe5085dbc626ab88d294ae18bb7951a435a27
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-10-08 20:51:07 +0000

    Added task 4 file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Add 2 positive numbers and print the result
+ *
+ * @argc: Counts all the arguments
+ * @argv: An array of arguments
+ *
+ * Return: 0 if successful: or 1 & print "Error"
+ */
+int main(int argc, char *argv[])
+{
+	int total;
+	int i;
+	char *p;
+	int num;
+
+	total = 0;
+	if (argc > 1)
+	{
+		for (i = 0; argv[i], i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				toral += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+	}
+	printf("%d\n", total);
+	return (0);
+}
