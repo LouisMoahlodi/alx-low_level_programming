03cb70fd7b0b3ebf39a5e20d7208610abdf8d29d
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-10-10 16:42:09 +0000

    Added task 3 file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Function returns a pointer to a 2D array of integers
+ *
+ * @width: Integer to display the rows
+ * @height: Integer to display columns
+ *
+ * Return: Double pointer:
+ * or "NULL" if failure: or if width or height is 0 || (-)
+ */
+int **alloc_grid(int width, int height)
+{
+	int **new;
+
+	int row, col, num;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	/*Intializing the 2D array */
+       int array2D[	
+}
---
+++ 3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABS_FUNCTION_LIKE_MACRO_H
+#define ABS_FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+
+#endif /* ABS_FUNCTION_LIKE_MACRO_H */
