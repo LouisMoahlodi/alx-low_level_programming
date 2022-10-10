5ea4ec0df123e58402d9b17d1b9178513e2c717c
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-10-10 19:20:43 +0000

    Added task 0 file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Poppy, learning to work with structs
+ *
+ * @name: Is character example for poppy
+ * @age: Is float for poppy age
+ * @owner: Is a charcter for poppy owner
+ *
+ * Description: Program is database for information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
