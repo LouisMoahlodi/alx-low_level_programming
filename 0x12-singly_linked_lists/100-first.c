ed1bfc0e60dd8f2854bad330472709a4bd936845
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-10-25 18:34:15 +0000

    Correction on task 5 file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+void __attribute__ ((constructor)) toofast();
+
 /**
  * toofast - function executed before main function is executed
  *
  * Return: void
  */
-void __attribute__ ((contstructor)) toofast()
+void toofast(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
