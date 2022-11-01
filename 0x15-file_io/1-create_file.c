71d3407957b63687b3f11f0e102aea800d15b8db
LouisMoahlodi <lebushomoahlodi@gmail.com>
2022-11-01 13:24:28 +0000

    Correction 2 on task 2 file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
