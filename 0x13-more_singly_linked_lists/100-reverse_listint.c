96f0e3a1ebbc260f7d434526d9922ca439d16c9c
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-01-11 08:27:37 +0200

    Minor fix 2

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,39 +1,64 @@
 #include "lists.h"
 
 /**
- * reverse_listint - Function revers a linked list
+ * reverse_listint - Function reverses a linked list
  *
- * @head: double pointer to the head the list
+ * @head: double pointer to the head of the list
  *
- * Return: pointer to first node if reversed list
+ * Return: pointer to the new head of the reversed list
  */
-
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *hold;
-	listint_t *current;
-
-	if (*head == NULL)
-		return (NULL);
+	listint_t *temp, *current;
 
 	current = *head;
-	*head = current->next;
-	hold = (*head)->next;
-	current->next = NULL;
-	if (*head == NULL)
+
+	while (current && (temp = current->next))
 	{
-		*head = current;
-		return (current);
+		current->next = temp->next;
+		temp->next = *head;
+		*head = temp;
 	}
 
-	while (hold != NULL)
-	{
-		(*head)->next = current;
-		current = *head;
-		*head = hold;
-		hold = (*head)->next;
-	}
-
-	(*head)->next = current;
-	return (*head);
+	return *head;
 }
+
+// #include "lists.h"
+
+// /**
+//  * reverse_listint - Function revers a linked list
+//  *
+//  * @head: double pointer to the head the list
+//  *
+//  * Return: pointer to first node if reversed list
+//  */
+
+// listint_t *reverse_listint(listint_t **head)
+// {
+// 	listint_t *hold;
+// 	listint_t *current;
+
+// 	if (*head == NULL)
+// 		return (NULL);
+
+// 	current = *head;
+// 	*head = current->next;
+// 	hold = (*head)->next;
+// 	current->next = NULL;
+// 	if (*head == NULL)
+// 	{
+// 		*head = current;
+// 		return (current);
+// 	}
+
+// 	while (hold != NULL)
+// 	{
+// 		(*head)->next = current;
+// 		current = *head;
+// 		*head = hold;
+// 		hold = (*head)->next;
+// 	}
+
+// 	(*head)->next = current;
+// 	return (*head);
+// }
